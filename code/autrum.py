6e9527ca084d8718a2c809a89f55b9b5ab8acf37
Jean Marco Rojas <jeanmarco1997@gmail.com>
2022-02-23 15:09:20 -0600

    Update autrum.py

--- code/autrum.py
+++ code/autrum.py
@@ -1,8 +1,1 @@
-from IPython.display import Image
-import numpy as np
-import scipy.fftpack as fourier
-import matplotlib.pyplot as plt
-import scipy.io.wavfile as waves
-import winsound
-
 print("hola mundo autrum!")
