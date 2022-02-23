48f178614909b8b110affec37d3b3e7ec515328a
Jean Marco Rojas <jeanmarco1997@gmail.com>
2022-02-23 14:57:39 -0600

    imports for Fourier

--- code/autrum.py
+++ code/autrum.py
@@ -1,1 +1,8 @@
+from IPython.display import Image
+import numpy as np
+import scipy.fftpack as fourier
+import matplotlib.pyplot as plt
+import scipy.io.wavfile as waves
+import winsound
+
 print("hola mundo autrum!")
